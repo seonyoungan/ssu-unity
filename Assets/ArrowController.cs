8c728ac9f02261f72c04b90702800696bfef0bf3
seonyoungan <asy093@naver.com>
2022-05-19 04:07:05 +0900

    update project  CatEscape_2

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    /*player를 불러왔으면 void Start() 또는
+     * void Awake()에서도 불러줘야한다. (awake가 Start보다 빠르긴함)*/
+
+   GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //낙하
+        transform.Translate(0, -0.05f, 0); //y축으로 -0.1씩 낙하
+
+        if(transform.position.y < -5.0f) //-5.0보다 더 작으면
+        {
+            Destroy(gameObject); //arrow가 사라진다.
+        }
+
+        //충돌판정
+        Vector2 p1 = transform.position;
+        Vector2 p2 = this.player.transform.position;
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.5f;
+        float r2 = 1.0f;
+
+        if(d < r1 + r2)
+        {
+            // 감독 스크립트에 플레이어와 충돌한 것을 전한다
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().DecreaseHp();
+
+            // 충돌한 경우는 화살을 지운다
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        //왼쪽화살표 누를 때
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-2, 0, 0);
+        }
+
+        //오른쪽화살표 누를 때
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(2, 0, 0);
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit(); //종료
+    }
+}
